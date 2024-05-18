7f0c300f7476caa0ef971cf4ba37224ae34cdea0
Matheus <teteusudo09@gmail.com>
2024-05-17 22:32:39 -0300

    tarefa

--- www/js/lerclientes.js
+++ www/js/lerclientes.js
@@ -1,23 +1,27 @@
-function lerclientes() 
+function lerclientes()
 {
     let requisicao = new XMLHttpRequest();
     let url = "https://etec.fernandograciano.com.br/listarclientes.php";
     requisicao.open("GET", url);
     requisicao.send();
-    requisicao.onload = function(){
+    requisicao.onload = function()
+    {
+        
         let corpotabela = document.querySelector("#corpotabela")
-        let dados = json.parse(this.response)
-        dados.forEach(function(valor,chave)
+        corpotabela.innerHTML = "";
+        let dados = JSON.parse(this.response)
+        console.log(dados)
+        dados.forEach( function (valor, chave)
         {
             let novalinha = document.createElement("tr")
             let celulaNome = document.createElement("td")
             let celulaCelular = document.createElement("td")
-            celulaNome.innerHTML = valor.celulanome
-            celulaCelular.innerHTML = valor.celulacelular
+            celulaNome.innerHTML = valor.nome
+            celulaCelular.innerHTML = valor.celular
             novalinha.append(celulaNome)
             novalinha.append(celulaCelular)
             corpotabela.append(novalinha)
         });
-        console.log(this.response)
-    }
-};
+        //console.log(this.response)
+    } 
+}
